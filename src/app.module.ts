811ab0a08b473f282ac469389e2d9ca1bc0a2c37
sandvik16 <sannndvik16@gmail.com>
2022-04-12 20:22:17 +0300

    feat: add TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ec2-99-80-170-190.eu-west-1.compute.amazonaws.com',
+      port: 5432,
+      username: 'uapmbqnuqsfuaj',
+      password:
+        '3b24f2ee46c04e7d2d37dbe9d4322abe5a99c36c6e42aa4a8e7589e7fb9a7751',
+      database: 'd97bef2n0571gv',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
