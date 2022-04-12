ff5a0da8af7cbba43c92d7003e32cd9cf69457e8
sandvik16 <sannndvik16@gmail.com>
2022-04-12 20:43:07 +0300

    try to connect db for heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,24 @@
+import { ConfigModule } from '@nestjs/config';
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    UsersModule,
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       url: process.env.DATABASE_URL,
       type: 'postgres',
-      host: 'ec2-99-80-170-190.eu-west-1.compute.amazonaws.com',
-      port: 5432,
-      username: 'uapmbqnuqsfuaj',
-      password:
-        '3b24f2ee46c04e7d2d37dbe9d4322abe5a99c36c6e42aa4a8e7589e7fb9a7751',
-      database: 'd97bef2n0571gv',
+      ssl: {
+        rejectUnauthorized: false,
+      },
       entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      synchronize: true, // This for development
+      autoLoadEntities: true,
     }),
   ],
   controllers: [AppController],
